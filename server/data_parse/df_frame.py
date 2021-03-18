3aba379f029673896bad44ee62ed1b83505e961a
YeonggilGo <dudrlf1859@naver.com>
2021-03-18 14:11:05 +0900

    [ADD] make kcq

--- server/data_parse/df_frame.py
+++ server/data_parse/df_frame.py
@@ -3,6 +3,8 @@ kw_columns = (
     "id",
     "content",
     "count"
+    "meaning",
+    "cpcq"
 )
 
 # cpcq: 구어체 말뭉치
@@ -10,4 +12,11 @@ cpcq_columns = (
     "id",
     "kor",
     "eng"
+)
+
+# kw_in_cpcq: 각 cpcq가 포함하는 kw
+kcq_columns = (
+    "id",
+    "kw_id",
+    "cpcq_id"
 )
