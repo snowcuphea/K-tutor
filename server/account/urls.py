cb43f84d5c0ce74c3796285ba0f615bb3e3a5003
YeonggilGo <dudrlf1859@naver.com>
2021-03-29 22:50:14 +0900

    [EDIT] edit account url

--- server/account/urls.py
+++ server/account/urls.py
@@ -4,7 +4,7 @@ from rest_framework_jwt.views import obtain_jwt_token
 from . import views
 
 urlpatterns = [
-    path('signup', views.UserViewSet.as_view({"post": "post"})),
+    path('signup', views.SignupViewSet.as_view({"post": "post"})),
     path('modify_delete', views.UserViewSet.as_view({"put": "put", "delete": "delete"})),
     path('api/token', obtain_jwt_token),
     path('login', views.LoginViewSet.as_view({"get": "get"}))
