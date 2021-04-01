2d9fceaa688a79f54f0fc4f09c97016736ee3cc0
YeonggilGo <dudrlf1859@naver.com>
2021-04-01 16:10:55 +0900

    [EDIT] set recent_cs default

--- server/account/views.py
+++ server/account/views.py
@@ -128,9 +128,12 @@ class LoginViewSet(viewsets.GenericViewSet,
         data['learned_lc_cnt'] = user.learned_lc.all().count()
         # recent_learned_lc = serializers.ListField()
         data['recent_learned_lc'] = list(user.learned_lc.all().order_by('-pk').values())
-        recent_cs = Cs.objects.get(pk=data['recent_learned_lc'][0]['cs_id']).__dict__
-        if recent_cs['type'] == 'kpop':
-            recent_cs['name'] = recent_cs['name'].split(' - ')[0]
+        if Lc.objects.filter(learned_user=user).exists():
+            recent_cs = Cs.objects.get(pk=data['recent_learned_lc'][0]['cs_id']).__dict__
+            if recent_cs['type'] == 'kpop':
+                recent_cs['name'] = recent_cs['name'].split(' - ')[0]
+        else:
+            recent_cs = Cs.objects.get(pk=2)
         data['recent_cs'] = recent_cs
         # recent_lc_progress = serializers.DictField()
         recent_lc_progress = dict()
