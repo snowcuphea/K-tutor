04076f9ebfe20b4ca29879f5c0c5615b6ea38529
YeonggilGo <dudrlf1859@naver.com>
2021-04-05 17:24:56 +0900

    [EDIT] edit attr

--- server/account/views.py
+++ server/account/views.py
@@ -222,30 +222,30 @@ class AchievementViewSet(viewsets.GenericViewSet,
         ac_manage = user.achieved.all()
         for acm in ac_manage:
             al = Achievement.objects.get(id=acm.achievement_id)
-            if al.done >= al.achievement.total:
+            if al.done >= al.total:
                 user_achievement.append({
-                    "achievement_id": al.achievement.id,
-                    "title": al.achievement.title,
-                    "content": al.achievement.content,
-                    "imgurl": al.achievement.imgurl,
-                    "done": al.done,
-                    "total": al.achievement.total,
-                    "great_kor": al.achievement.great_kor,
-                    "great_eng": al.achievement.great_eng,
-                    "great_dsc": al.achievement.great_dsc,
+                    "achievement_id": al.id,
+                    "title": al.title,
+                    "content": al.content,
+                    "imgurl": al.imgurl,
+                    "done": acm.done,
+                    "total": al.total,
+                    "great_kor": al.great_kor,
+                    "great_eng": al.great_eng,
+                    "great_dsc": al.great_dsc,
                     "status": 1
                 })
             else:
                 user_achievement.append({
-                    "achievement_id": al.achievement.id,
-                    "title": al.achievement.title,
-                    "content": al.achievement.content,
-                    "imgurl": al.achievement.imgurl,
-                    "done": al.done,
-                    "total": al.achievement.total,
-                    "great_kor": al.achievement.great_kor,
-                    "great_eng": al.achievement.great_eng,
-                    "great_dsc": al.achievement.great_dsc,
+                    "achievement_id": al.id,
+                    "title": al.title,
+                    "content": al.content,
+                    "imgurl": al.imgurl,
+                    "done": acm.done,
+                    "total": al.total,
+                    "great_kor": al.great_kor,
+                    "great_eng": al.great_eng,
+                    "great_dsc": al.great_dsc,
                     "status": 0
                 })
 
