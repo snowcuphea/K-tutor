d3152d7443cc01d5ea7d497421da0c1b5d38b4c3
YeonggilGo <dudrlf1859@naver.com>
2021-04-07 17:21:32 +0900

    [EDIT] change domain

--- server/athorize.sh
+++ server/athorize.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains="j4a303.p.ssafy.io.com"
+domains="j4a303.p.ssafy.io"
 rsa_key_size=4096
 data_path="../volumes/certbot"
 email="dudrlf1859@naver.com" # Adding a valid address is strongly recommended
