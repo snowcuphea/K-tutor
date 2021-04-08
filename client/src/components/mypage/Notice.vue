8c4aab99645bc75200ca1f03a73918c7a3e2e441
김민정 <snowcuphea@gmail.com>
2021-04-08 17:20:19 +0900

    [EDIT] 공지사항 수정

--- client/src/components/mypage/Notice.vue
+++ client/src/components/mypage/Notice.vue
@@ -16,6 +16,8 @@
         </v-btn>
 
 
+        <h1 class="px-3">Version</h1>
+        <h3 class="px-6 pb-5"> Ver.{{ noticeItems_version }} - (04/08/2021 updated)  </h3>
         <h1 class="px-3">Greetings</h1>
         <h3 class="px-6 pb-5"> {{ noticeItems_kr }}  </h3>
         <h3 class="px-6 pt-5"> {{ noticeItems_eng }}  </h3>
@@ -31,6 +33,7 @@
 export default {
   props: ['showNotice'],
   data: () => ({
+    noticeItems_version: "1.0.0",
     noticeItems_kr: "K-tutor(말뭉치)는 한국의, 말뭉치 데이터('malmoongchi')를 \
         활용하여 제작한 서비스입니다. 말뭉치는 언어 연구를 위하여 컴퓨터가 \
         텍스트를 가공, 처리, 분석할 수 있는 형태로 모아 놓은 자료의 집합, 말모둠, 글모둠을 뜻합니다.",
