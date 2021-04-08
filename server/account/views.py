62e8c14a60590109ebb092302ba60664c1b7b724
YeonggilGo <dudrlf1859@naver.com>
2021-04-08 22:56:36 +0900

    Update views.py

--- server/account/views.py
+++ server/account/views.py
@@ -131,7 +131,11 @@ class LoginViewSet(viewsets.GenericViewSet,
         # learned_lc_cnt = serializers.IntegerField()
         data['learned_lc_cnt'] = user.learned_lc.all().count()
         # recent_learned_lc = serializers.ListField()
-        data['recent_learned_lc'] = RecentLearnedLc.objects.filter(user=user).order_by('learned_at').values()
+        pk_list = RecentLearnedLc.objects.filter(user=user).order_by('learned_at').values_list('lc_id', flat=True)
+        ordering = 'FIELD(`id`, %s)' % ','.join(str(id) for id in pk_list)
+        data['recent_learned_lc'] = Lc.objects.filter(id__in=pk_list).extra(
+            select={'ordering': ordering}, order_by=('ordering',)
+        ).values()
 
         if Lc.objects.filter(learned_user=user).exists():
             recent_cs = Cs.objects.get(pk=data['recent_learned_lc'][0]['cs_id']).__dict__
